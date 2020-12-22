81160d734d66ce5f3994b20188f44db0e896d85a
SHOMANS <mohammedshoman9@gmail.com>
2020-12-22 15:25:08 +0200

    add slider data without make slider

--- src/pages/VideosPage/index.jsx
+++ src/pages/VideosPage/index.jsx
@@ -2,8 +2,18 @@ import Footer from "../../components/Footer";
 import NavBar from "../../components/NavBar";
 import Links from "../../components/Links";
 import { SmallPlayBtn } from "../../components/PlayBtn";
-import { H4, HWB } from "../../components/Typography";
+import { H4, H5, HWB } from "../../components/Typography";
+import { Card } from "../../components/Card";
 import * as S from "./style";
+
+const data = [
+  { imgSrc: "", id: 1, content: "", date: "", time: "" },
+  { imgSrc: "", id: 2, content: "", date: "", time: "" },
+  { imgSrc: "", id: 3, content: "", date: "", time: "" },
+  { imgSrc: "", id: 4, content: "", date: "", time: "" },
+  { imgSrc: "", id: 5, content: "", date: "", time: "" },
+  { imgSrc: "", id: 6, content: "", date: "", time: "" },
+];
 
 export default function VideosPage() {
   return (
@@ -24,6 +34,20 @@ export default function VideosPage() {
                   <li> Dec 17, 2019</li>
                 </ul>
               </H4>
+            </div>
+          </div>
+
+          <div className="slider">
+            <div className="slides">
+              {data.map((slide) => (
+                <Card>
+                  <H5>{slide.content}</H5>
+                  <ul>
+                    <li>{slide.time}</li>
+                    <li>{slide.date}</li>
+                  </ul>
+                </Card>
+              ))}
             </div>
           </div>
         </S.Wrapper>
